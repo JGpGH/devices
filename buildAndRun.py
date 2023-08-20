db1e9d42743f87cdfe2a4084a98457559bae3b3a
JGpGH <j.gauthier130@gmail.com>
2023-08-20 01:52:20 -0400

    configs for board

--- buildAndRun.py
+++ buildAndRun.py
@@ -1,15 +1,29 @@
 import os
 import sys
+import json
 
 def buildAndRun(program, fbqn, port):
     os.system(f"arduino-cli compile --fqbn {fbqn} {program}")
     os.system(f"arduino-cli upload -p {port} --fqbn {fbqn} {program}")
 
+def loadConfigurations():
+    with open('configs.json') as json_file:
+        data = json.load(json_file)
+        return data
+
 def main():
-    program = sys.argv[1]
-    fbqn = sys.argv[2]
-    port = sys.argv[3]
-    buildAndRun(program, fbqn, port)
+    configs = loadConfigurations()
+    if len(sys.argv) < 3:
+        print("Please provide a config id and a port")
+        return
+    configId = sys.argv[1]
+    port = sys.argv[2]
+    if configId not in configs:
+        print("Invalid config id")
+        return
+    config = configs[configId]
+    print(f"Building and running {config['name']} on {port}")
+    buildAndRun(config['program'], config['fbqn'], port)
 
 if __name__ == "__main__":
     main()
